63d761ded903ede0c494f5c13f8a62e4d14bf973
MurpCon28 <conormu4phy@gmail.com>
2021-02-18 12:51:40 +0000

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <br>
+    This is a home page!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+}
+</style>
